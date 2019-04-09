bd04fa16e1abcbb8e1a4c24910aa929933d9c7d4
Hamza Amer <hamer1563@gmail.com>
2019-04-09 18:04:58 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+//This js file is the main running server
+
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/',(req, res)=>{
+	res.render('index');
+})
+
+
+
+app.listen(3000,()=>{
+	console.log('Server is now running...')
+});
