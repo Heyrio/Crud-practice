21804e29a1bd18f4eb77ce6562033ba256f32d96
Hamza Amer <hamer1563@gmail.com>
2019-04-16 16:51:32 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,17 +3,35 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const bodyParser = require('body-parser');
+const base = require('./base.js');
 
 
+
+//including middleware so we can parse post requests
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+
+// Template engine for rendering is EJS
 app.set('view engine', 'ejs');
+
+//Specifies the directory of the static files to serve
 app.use(express.static(__dirname + '/public/'));
 
+//rendering the main home page
 app.get('/',(req, res)=>{
 	res.render('index');
+	
 })
 
+app.post('/',(req, res)=>{
+	var userName = req.body;
+	base.userDataFinal(userName);
+	res.render('index');
+});
 
-
+//Opens a port to listen on
 app.listen(3000,()=>{
 	console.log('Server is now running...')
 });
